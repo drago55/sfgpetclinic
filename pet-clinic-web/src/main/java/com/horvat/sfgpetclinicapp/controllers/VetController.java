d04f432d061b0f2ca9e97ac9ba27cb6ddb3a8a41
Dragutin Horvat <dhorvat22@gmail.com>
2020-09-19 19:25:38 +0200

    added vets list

--- pet-clinic-web/src/main/java/com/horvat/sfgpetclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/horvat/sfgpetclinicapp/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.horvat.sfgpetclinicapp.controllers;
 
+import com.horvat.sfgpetclinicapp.services.VetServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping(path = {"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    private final VetServices vetServices;
+
+    public VetController(VetServices vetServices) {
+        this.vetServices = vetServices;
+    }
+
+    @RequestMapping(path = {"", "/", "/index", "/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetServices.findAll());
         return "vets/index";
     }
 
